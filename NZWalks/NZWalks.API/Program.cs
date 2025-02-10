ca2099901c660bed4fbe48f5e75d0b3896addb35
Michael Manley <manleym362@gmail.com>
2025-02-10 14:45:09 +0200

    Added and implemented an interface, decoupling the controller from the database

--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
+using NZWalks.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,13 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer(); //ApiExplorer service injection into Dependency Injection container.
-builder.Services.AddSwaggerGen(); //SwaggerGen service injection into Dependency Injection container.
+builder.Services.AddEndpointsApiExplorer(); // ApiExplorer service injection into Dependency Injection container.
+builder.Services.AddSwaggerGen(); // SwaggerGen service injection into Dependency Injection container.
 
 builder.Services.AddDbContext<NZWalksDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString"))); /* DbContext class injected into Dependency Injection container with connection string.
                                                                                                 The application will manage all the instances of this DbContext class whenever we 
                                                                                                 call it within controllers or repositories. */
+
+builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>(); // We inject the IRegionRepository into the container, with the implmentation of SQLRegionRepository.
 
 var app = builder.Build();
 
