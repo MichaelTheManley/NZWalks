a2e2408a16d3fcffe1167bef797dbfec237e83b7
Michael Manley <manleym362@gmail.com>
2025-02-13 15:25:28 +0200

    Created interface method for creating a walk

--- NZWalks/NZWalks.API/Repositories/SQLWalkRepository.cs
+++ NZWalks/NZWalks.API/Repositories/SQLWalkRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace NZWalks.API.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
 {
-    public class SQLWalkRepository
+    public class SQLWalkRepository : IWalkRepository
     {
+        private readonly NZWalksDbContext dbContext;
+
+        public SQLWalkRepository(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<Walk> CreateWalkAsync(Walk walk)
+        {
+            await dbContext.Walks.AddAsync(walk);
+            await dbContext.SaveChangesAsync();
+            return walk;
+        }
     }
 }
