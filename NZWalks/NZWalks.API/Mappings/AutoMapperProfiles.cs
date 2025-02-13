11bf83b94485bcd94a53515e0c0ec78742acafe6
Michael Manley <manleym362@gmail.com>
2025-02-13 17:10:56 +0200

    Added mapping for update dto back to walk

--- NZWalks/NZWalks.API/Mappings/AutoMapperProfiles.cs
+++ NZWalks/NZWalks.API/Mappings/AutoMapperProfiles.cs
@@ -14,6 +14,7 @@ namespace NZWalks.API.Mappings
             CreateMap<UpdateRegionRequestDto, Region>().ReverseMap();
             CreateMap<AddWalkRequestDto, Walk>();
             CreateMap<Walk, WalkDto>();
+            CreateMap<UpdateWalkRequestDto, Walk>().ReverseMap();
         }
     }
 }
