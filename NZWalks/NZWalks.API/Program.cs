d53dd692c4e0f3c2024108ae871f0fda1ccf0c93
Michael Manley <manleym362@gmail.com>
2025-02-24 12:41:05 +0200

    Added JWT Authentication; Looking to implement registering and logging in methods

--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
 using NZWalks.API.Mappings;
 using NZWalks.API.Repositories;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +24,21 @@ builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>(); // We inje
 builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>(); // We inject the IWalkRepository into the container, with the implmentation of SQLWalkRepository.
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
+// We add the authentication service.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) 
+    .AddJwtBearer(options =>
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"], // Reads it from the appsettings.json file
+        ValidAudience = builder.Configuration["Jwt:Audience"], // Reads it from the appsettings.json file
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    }); 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,7 +50,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseAuthentication(); // This must be before UseAuthorization
+app.UseAuthorization(); // Before this can happen, we need to authenticate!
 
 app.MapControllers();
 
