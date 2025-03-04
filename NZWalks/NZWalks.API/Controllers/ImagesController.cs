5313240347883acbca72953222510312beb921b9
Michael Manley <manleym362@gmail.com>
2025-03-04 15:40:54 +0200

    Image Controller boilerplate code added; Looking to finish UploadImage

---
+++ NZWalks/NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO.Image;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> UploadImage([FromForm] ImageUploadRequestDto imageDetails)
+        {
+            // We first need to validate if the request is correct or not
+            ValidateFileUpload(imageDetails);
+
+            if (ModelState.IsValid)
+            {
+                // User repo to upload image if valid
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto imageDetails)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if (!allowedExtensions.Contains(Path.GetExtension(imageDetails.File.FileName)))
+            {
+                ModelState.AddModelError("File", "Unsupported file type. Only .jpg, .jpeg, .png files are allowed.");
+            }
+
+            if (imageDetails.File.Length > 10485760) // 10 mb to bytes
+            {
+                ModelState.AddModelError("File", "Image file is too big. File size more than 10mb.");
+            }
+        }
+    }
+}
